0819ecb1b809f013386e763734ff6a16d1353f5f
WelldoneM <welldonematanga12@gmail.com>
2024-10-23 12:06:54 -0500

    add style

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
+import './CourseList.css'
+import { Card } from 'react-bootstrap';
 
 const CourseList = ({ courses }) => (
-  <ul>
+  <div className="course-list">
     {Object.keys(courses).map((courseKey) => {
       const course = courses[courseKey];
       return (
-        <li key={courseKey}>
-          <p>{course.term} {course.number}: {course.title}</p>
-        </li>
+        <Card key={courseKey} className="course-card">
+          <Card.Body className="d-flex flex-column">
+            <Card.Title><strong>{course.term} {course.number}</strong></Card.Title>
+            <Card.Text>{course.title}</Card.Text>
+          </Card.Body>
+          <div className="card-footer">
+              {course.meets}
+            </div>
+        </Card>
       );
     })}
-  </ul>
+  </div>
 );
 
 export default CourseList;
